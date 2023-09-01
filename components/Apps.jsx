69343e7fa198bd57f8290352218087419c748a62
Krunal <126875304+krunalbhongade@users.noreply.github.com>
2023-09-01 07:30:01 +0530

    Initial commit

---
+++ components/Apps.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Headings from "./Headings";
+
+function Apps() {
+  return (
+    <div>
+      <Headings />
+    </div>
+  );
+}
+
+export default Apps;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Apps from "../components/Apps";
+
+ReactDOM.render(<Apps />, document.getElementById("root"));
